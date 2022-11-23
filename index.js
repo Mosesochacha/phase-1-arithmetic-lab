34f43a5c142854aa10fd98aea53c81e8947023d8
mosesochacha <ochachamoses443@gmail.com>
2022-11-23 15:46:32 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 =2
+const num2 =31
+const multiply=num1*num2
 
-// Write your code here
+let random =2;
+Math.floor(Math.random() * 10) + 1;
+ let max = 20
+ Math.max(0,2)
+ let num3=29
+ let num4=5
+ let mod =(num3%=num4)
